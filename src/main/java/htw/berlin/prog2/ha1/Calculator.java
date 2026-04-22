6b1c4d2605905806607df7cd8ef8621490390a66
toprak <toprak.gueven@student.htw-berlin.de>
2026-04-22 15:51:15 +0200

    teilaufgabe 3 bug fixes

--- src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -81,7 +81,7 @@ public class Calculator {
             default -> throw new IllegalArgumentException();
         };
         screen = Double.toString(result);
-        if(screen.equals("NaN")) screen = "Error";
+        if(screen.equals("NaN") || screen.equals("Infinity")) screen = "Error";
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
 
     }
@@ -123,6 +123,7 @@ public class Calculator {
             case "-" -> latestValue - Double.parseDouble(screen);
             case "x" -> latestValue * Double.parseDouble(screen);
             case "/" -> latestValue / Double.parseDouble(screen);
+            case "%" -> latestValue % Double.parseDouble(screen);
             default -> throw new IllegalArgumentException();
         };
         screen = Double.toString(result);
